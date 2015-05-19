f3927fb8d5fb655ff3b6395338e643241687ceb6
Vishnu Ks <yo@vishnuks.com>
2015-05-20 03:46:06 +0530

    json completed

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
 <?php
 
-$url = 'http://en.wikipedia.org/wiki/Special:RandomInCategory/Chemistry';	
+$category = $_GET["category"];
+//$category = "Chemistry";
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0"); // Necessary. The server checks for a valid User-Agent.
-curl_exec($ch);
+$url = 'http://en.wikipedia.org/wiki/Special:RandomInCategory/'.$category;	
 
-$response = curl_exec($ch);
-preg_match_all('/^Location:(.*)$/mi', $response, $matches);
-curl_close($ch);
+$page = 'Category';
 
-echo !empty($matches[1]) ? trim($matches[1][0]) : 'No redirect found';
-?>
+while (strpos($page, 'Category') !== false) {
+	//	print $page."\n";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0"); // Necessary. The server checks for a valid User-Agent.
+
+
+	curl_exec($ch);
+
+	$response = curl_exec($ch);
+	preg_match_all('/^Location:(.*)$/mi', $response, $matches);
+	curl_close($ch);
+
+	if (!empty($matches[1])) {
+		$temp = trim($matches[1][0]);
+		$page = str_replace("http://en.wikipedia.org/wiki/", "", $temp);
+	//	echo $page;
+
+	}
+
+
+	
+
+}
+
+if (!empty($matches[1])) {
+		$temp = trim($matches[1][0]);
+		$page = str_replace("http://en.wikipedia.org/wiki/", "", $temp);
+		$arr = array('category' => $page);
+		$json = json_encode($arr);
+		print $json;
+
+	}
+
