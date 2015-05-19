93cd0bb4a8f98ec5a6b27737107be062d1d2a753
Vishnu Ks <yo@vishnuks.com>
2015-05-20 03:12:56 +0530

    version 1

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<?php
 
-<script>
-pageId =  25675557;
-  urlContent = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=categories&pageids="+pageId+"&continue=&callback=?"
-  $.getJSON(urlContent, function(data) {
-        $.each(data.query.pages, function(i, item) {
-          $.each(item.categories, function(j, cat) {
-            console.log(cat.title);
-          });
-          
-        });
-  });
+$url = 'http://en.wikipedia.org/wiki/Special:RandomInCategory/Chemistry';	
 
-</script>
-</head>
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0"); // Necessary. The server checks for a valid User-Agent.
+curl_exec($ch);
 
+$response = curl_exec($ch);
+preg_match_all('/^Location:(.*)$/mi', $response, $matches);
+curl_close($ch);
+
+echo !empty($matches[1]) ? trim($matches[1][0]) : 'No redirect found';
+?>
