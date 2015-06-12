6858af8f3098f8e862a3b1c48ad733dd935ac529
Vishnu Ks <yo@vishnuks.com>
2015-06-12 16:29:42 +0530

    getredirect updated

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-
 if(!isset($_GET["category"])) {
 	exit();
 }
 
-$category = $_GET["category"];
-//$category = "Chemistry";
+//$category = $_GET["category"];
+
+
+function getRedirectUrl ($url) {
+    stream_context_set_default(array(
+        'http' => array(
+            'method' => 'HEAD'
+        )
+    ));
+    $headers = get_headers($url, 1);
+    if ($headers !== false && isset($headers['Location'])) {
+        return $headers['Location'];
+    }
+    return false;
+}
+
+$category = "Chemistry";
 
 
 $url = 'http://en.wikipedia.org/wiki/Special:RandomInCategory/'.$category;	
@@ -13,43 +27,29 @@ $url = 'http://en.wikipedia.org/wiki/Special:RandomInCategory/'.$category;
 $page = 'Category';
 
 while (strpos($page, 'Category') !== false or strpos($page, 'Portal') !== false) {
-	//	print $page."\n";
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0"); // Necessary. The server checks for a valid User-Agent.
-
-
-	curl_exec($ch);
-
-	$response = curl_exec($ch);
-	preg_match_all('/^Location:(.*)$/mi', $response, $matches);
-	curl_close($ch);
-
-	if (!empty($matches[1])) {
-		$temp = trim($matches[1][0]);
-		$page = str_replace("http://en.wikipedia.org/wiki/", "", $temp);
-	//	echo $page;
-
+	
+	$temp = getRedirectUrl($url);
+	print $temp[1];
+	
+	if($temp != false) {
+		$page = $temp[1];
+		$page = trim($page);
+		$page = str_replace("http://en.wikipedia.org/wiki/", "", $page);
 	}
 
 	else {
+		$page = false;
 		break;
 	}
 
-
-	
-
 }
 
-if (!empty($matches[1])) {
-		$temp = trim($matches[1][0]);
-		$page = str_replace("http://en.wikipedia.org/wiki/", "", $temp);
-		//$arr = array('category' => $page);
-		//$json = json_encode($arr);
-		//print $json;
-		echo $_GET['callback'] . '(' . "{'category' : '$page' }" . ')';
+
+
+if ($page != false) {
+		$temp2 = trim($page);
+		$page2 = str_replace("http://en.wikipedia.org/wiki/", "", $temp2);
+		echo $_GET['callback'] . '(' . "{'category' : '$page2' }" . ')';
 
 	}
 
